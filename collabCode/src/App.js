a3e66138e4c1f2b6829c4b44b0fa435315136f05
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2021-08-13 14:19:13 +0100

    Css changes to header

--- collabCode/src/App.js
+++ collabCode/src/App.js
@@ -5,15 +5,19 @@ import 'react-notifications-component/dist/theme.css'
 import Login from './components/Login/Login'
 import Editor from './components/Editor/Editor'
 
+import './App.scss'
+
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <Router>
       <ReactNotification />
       <Switch>
         <Route path="/" component={Login} exact />
         <Route path="/:id" component={Editor} exact />
       </Switch>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
--- collabCode/src/components/Header/Header.js
+++ collabCode/src/components/Header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <h1>CollabCode</h1>
-            <h5>Collaborate on code with your friends in real time</h5>
+            <h3>Collaborate on code with your friends in real time</h3>
         </header>
     )
 }
